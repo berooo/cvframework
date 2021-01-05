cdb9c65f324ea2164b44c7bf1938dffb0ac256ec
berooo <1819246385@qq.com>
2021-01-05 20:29:20 +0800

    contrasive

--- train/retrieval/traincontrasive.py
+++ train/retrieval/traincontrasive.py
@@ -8,12 +8,11 @@
 import os
 import sys
 
-from train.adjustLR import _learning_rate_schedule
-
 BASE = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 sys.path.insert(0, BASE)
 print(BASE,flush=True)
 
+from train.adjustLR import _learning_rate_schedule
 from datasets.CartoonDataset import CartoonDataset
 import argparse
 import time
