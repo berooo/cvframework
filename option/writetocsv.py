d12b36427b9986055a937888f4151b9df144e49a
berooo <1819246385@qq.com>
2021-01-06 23:18:30 +0800

    csv

--- option/writetocsv.py
+++ option/writetocsv.py
@@ -8,10 +8,9 @@ import csv
 #映射
 #将文件名写到csv中
 
-gallery_path='../data/cartoon/gallery.txt'
-query_path='../data/cartoon/query.txt'
-imgdir='/home/shibaorong/cartoon/extraction/data/cartoontest'
-matpath='../extract/features/cartoon.mat'
+gallery_path='../datasets/data/gd/gallery.txt'
+query_path='../datasets/data/gd/query.txt'
+imgdir='../datasets/data/test'
 tofile='submission.csv'
 
 def get_img_name(path):
@@ -19,23 +18,6 @@ def get_img_name(path):
     for line in open(path):
         ids.append(line.strip())
     return ids
-
-def search(feature_path):
-    features = loadmat(feature_path)
-    Q = features['C']
-    X = features['P']
-
-    dis = np.zeros([X.shape[0], Q.shape[0]])
-
-    for j in range(Q.shape[0]):
-        d = (X - np.reshape(Q[j, :], (1, Q.shape[1]))) ** 2
-        disj = np.sum(d, axis=1)
-        dis[:, j] = disj
-    ranks = np.argsort(dis, axis=0)
-    '''sim = np.dot(X, Q.T)
-    ranks = np.argsort(-sim, axis=0)'''
-    np.save("ranks.npy", ranks)
-    return ranks
 
 def getresult(ranks,gallerys,querys):
     reslist=[]
@@ -57,6 +39,6 @@ def write_to_csv(res,tofile):
 if __name__=='__main__':
     querys = get_img_name(query_path)
     gallerys = get_img_name(gallery_path)
-    ranks=search(matpath)
+    ranks=np.load('../train/multimodal/ranks.npy')
     res=getresult(ranks,gallerys,querys)
     write_to_csv(res,tofile)
