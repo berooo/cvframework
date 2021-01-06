399549e8f19467079b202b8e1a60b3a09b4242a0
berooo <1819246385@qq.com>
2021-01-06 21:12:41 +0800

    config

--- core/checkpoint.py
+++ core/checkpoint.py
@@ -10,9 +10,8 @@
 import os
 import copy
 
-import core.distributed as dist
 import torch
-from core.config import cfg
+from core import cfg
 
 
 # Common prefix for checkpoint file names
@@ -48,27 +47,6 @@ def has_checkpoint():
         return False
     return any(_NAME_PREFIX in f for f in os.listdir(checkpoint_dir))
 
-
-def save_checkpoint(model, optimizer, epoch):
-    """Saves a checkpoint."""
-    # Save checkpoints only from the master process
-    if not dist.is_master_proc():
-        return
-    # Ensure that the checkpoint dir exists
-    os.makedirs(get_checkpoint_dir(), exist_ok=True)
-    # Omit the DDP wrapper in the multi-gpu setting
-    sd = model.module.state_dict() if cfg.NUM_GPUS > 1 else model.state_dict()
-    # Record the state
-    checkpoint = {
-        "epoch": epoch,
-        "model_state": sd,
-        "optimizer_state": optimizer.state_dict(),
-        "cfg": cfg.dump(),
-    }
-    # Write the checkpoint
-    checkpoint_file = get_checkpoint(epoch + 1)
-    torch.save(checkpoint, checkpoint_file)
-    return checkpoint_file
 
 
 def load_checkpoint(checkpoint_file, model, optimizer=None):
